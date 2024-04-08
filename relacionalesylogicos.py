fd11658b8420bb5ec8216f18b80be99a422b828d
WilmerGelves <125094214+WilmerGelves@users.noreply.github.com>
2024-04-07 23:00:04 -0500

    Operadores relacionales y lógicos

---
+++ relacionalesylogicos.py
@@ -0,0 +1,14 @@
+#Operadores relacionales
+
+# == ->igual que 
+# != ->diferente 
+# <  ->menor
+# >  ->mayor
+# <= ->menor-igual
+# >= ->mayor-igual
+
+#Operadores lógicos
+
+#and (y)
+#or (o)
+#xor (excluyente)
