e3a67728f03cac18ffc1ec7c7ae8ab30a0a2707d
WilmerGelves <125094214+WilmerGelves@users.noreply.github.com>
2024-04-11 07:10:22 -0500

    Funciones predefinidas y funciones con cadenas

---
+++ funciones.py
@@ -0,0 +1,25 @@
+""" Una función, es la forma de agrupar expresiones y sentencias (algoritmos) que realicen determinadas acciones, pero que éstas solo se ejecuten cuando son llamadas."""
+
+#Ejemplo: la funcion puede retornar un valor y éste se puede almacenar en una variable para visualizarlo.
+
+def saludo1():
+    print("Hola mundo")
+
+saludo1()
+
+#ó
+def saludo():
+    return "Bienvenido al sistema"
+
+
+saludo = saludo()
+print(saludo)
+
+
+
+#DEFINICION DE FUNCIONES CON PARÁMETROS
+def mi_funcion(nombre,apellido):
+    nombre_completo = nombre, apellido
+    print(nombre_completo)
+
+print(mi_funcion('Wilmer','Gelves'))
---
+++ funciones_con_cadenas.py
@@ -0,0 +1,7 @@
+saludo = "Hola mundo"
+
+print(len(saludo))#La funcion len sirve para ver la longitud de una variable.
+print(saludo.upper()) #Mayus
+print(saludo.lower()) #Minusc
+print(saludo.title()) #Letra título
+print(saludo.strip()) #elimaina espacios en blanco
